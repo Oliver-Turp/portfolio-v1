4fdf0da18f0fb664ecc83eab0ba5ca15b5e1b04d
Oliver-Turp <tcgamingbusiness@gmail.com>
2024-01-01 23:31:18 +0000

    Basic file structure achieved

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,7 +1,7 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
+import Header from "@/components/header/header";
+import Footer from "@/components/footer/footer";
 
-const inter = Inter({ subsets: ["latin"] });
+import "./globals.css";
 
 export const metadata = {
   title: "Page Title Here",
@@ -11,7 +11,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <Header />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
