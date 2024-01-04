be9716b4af47d6061c9ae561e00d14f9a0e2068d
Oliver-Turp <tcgamingbusiness@gmail.com>
2024-01-04 13:23:55 +0000

    Header skeleton done

--- frontend/src/components/header/header.js
+++ frontend/src/components/header/header.js
@@ -1,5 +1,37 @@
+import Image from "next/image";
+import Link from "next/link";
+import {
+  LogoLeaves as Logo,
+  LogoTreeCPU,
+  LogoTreeCyberPunk,
+  LogoTreeMan,
+  LogoTreePower,
+} from "@/assets/images/exports";
+
+import styles from "./header.module.css";
+
 const Header = () => {
-  return <header></header>;
+  return (
+    <header className={styles.headerWrap}>
+      <div className={styles.logoWrap}>
+        <Image src={Logo} alt="logo" />
+      </div>
+      <nav className={styles.navWrap}>
+        <Link href={`/`} className={styles.navLink}>
+          Link
+        </Link>
+        <Link href={`/`} className={styles.navLink}>
+          Link
+        </Link>
+        <Link href={`/`} className={styles.navLink}>
+          Link
+        </Link>
+        <Link className={styles.navBtn} href={`/`}>
+          Link
+        </Link>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
