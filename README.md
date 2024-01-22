d9f9f1201eaa9d7e714a1a4d83111dc7d520da34
Oliver-Turp <tcgamingbusiness@gmail.com>
2024-01-22 17:26:32 +0000

    Introduced framer motion skeleton

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 
 ## To Do list:
 
-`npm i framer-motion`
+- [x] `npm i framer-motion`
 
-- Redo the navigation HTML so that there is a mobile and desktop version. This way one can have framer motion and the other can be free. Pay attention to CSS when messing with this.
+- [x] Redo the navigation HTML so that there is a mobile and desktop version. This way one can have framer motion and the other can be free. Pay attention to CSS when messing with this.
 
-- Match up the timings for the mobile menu coming up with the animation time for the burger.
+- [ ] Match up the timings for the mobile menu coming up with the animation time for the burger.
 
-- Add in some fancy movements on the mobile menu. Perhaps the borders come in from the side one by one? May have to use :after elements.
+- [ ] Add in some fancy movements on the mobile menu. Perhaps the borders come in from the side one by one? May have to use :after elements.
--- archive/src/components/header/header.js
+++ archive/src/components/header/header.js
@@ -1,20 +1,82 @@
 "use client";
 
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import Lottie from "lottie-react";
+import { useAnimate, stagger } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
-import {
-  LogoLeaves as Logo,
-  Hamburger,
-} from "@/assets/exports";
+import { LogoLeaves as Logo, Hamburger } from "@/assets/exports";
 import styles from "./header.module.css";
 
+function useMenuAnimation(isOpen) {
+  const [scope, animate] = useAnimate();
+  const navDuration = 1.5;
+  const linkDelay = 0.13 * navDuration;
+  const linkStagger = 0.23 * navDuration;
+  const staggerMenuOpen = stagger(linkStagger);
+  const staggerMenuClose = stagger(linkDelay);
+
+  useEffect(() => {
+    const openSequence = [
+      // nav moves up
+      [
+        "nav",
+        { opacity: 1, y: 0 },
+        { duration: navDuration, ease: "easeInOut" },
+      ],
+      // links move right
+      [
+        "span a",
+        { x: "-50%" },
+        { duration: navDuration / 2, ease: "easeInOut", at: linkDelay },
+      ],
+      // links fade in
+      [
+        "span a",
+        { x: 0, opacity: 1, scale: 1, filter: "blur(0px)" },
+        {
+          duration: linkStagger,
+          delay: staggerMenuOpen,
+          ease: "easeInOut",
+        },
+      ],
+    ];
+
+    const closeSequence = () => {
+      // links move left and fade
+      animate(
+        "span a",
+        { x: "-100%", scale: 0.3, filter: "blur(20px)" },
+        {
+          duration: linkStagger * 2,
+          ease: "easeInOut",
+          delay: staggerMenuClose,
+        }
+      );
+      // nav retreats
+      animate(
+        "nav",
+        { opacity: 0, y: "100%" },
+        { duration: navDuration / 2, ease: "easeInOut", delay: linkStagger }
+      );
+    };
+
+    if (isOpen) {
+      animate(openSequence);
+    } else {
+      closeSequence();
+    }
+  }, [isOpen]);
+
+  return scope;
+}
+
 const Header = () => {
+  const burgerRef = useRef();
   const [currentTime, setCurrentTime] = useState(0);
   const [isPlaying, setIsPlaying] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
-  const burgerRef = useRef();
+  const scope = useMenuAnimation(isOpen);
 
   const handleClick = () => {
     const currentFrame = Math.round(
@@ -33,8 +95,8 @@ const Header = () => {
   const handleEnterFrame = (e) => {
     setCurrentTime(e.currentTime);
     const currentTime = Math.round(e.currentTime);
-    console.log(`currentTime`, currentTime);
-    console.log(`isPlaying`, isPlaying);
+    // console.log(`currentTime`, currentTime);
+    // console.log(`isPlaying`, isPlaying);
     console.log(`isOpen`, isOpen);
 
     // FIRST CLICK SETPLAYING AND OPEN TO TRUE
@@ -59,20 +121,7 @@ const Header = () => {
       <div className={styles.logoWrap}>
         <Image src={Logo} alt="logo" />
       </div>
-      <Lottie
-        lottieRef={burgerRef}
-        loop={false}
-        autoplay={false}
-        animationData={Hamburger}
-        onClick={handleClick}
-        onEnterFrame={handleEnterFrame}
-        className={styles.hamburger}
-      />
-      <nav
-        className={`${styles.navWrap} ${
-          isOpen ? styles.burgerOpen : styles.burgerClosed
-        }`}
-      >
+      <nav className={styles.desktopNav}>
         <Link href={`/`} className={styles.navLink}>
           Websites
         </Link>
@@ -85,17 +134,46 @@ const Header = () => {
         <Link className={styles.navBtn} href={`/`}>
           Contact
         </Link>
-        <section className={styles.mobContact}>
-          <h3>Oliver Turp</h3>
-          <section>
-            <p>email here</p>
-            <p>more text</p>
-          </section>
-          <Link className={styles.mobBtn} href={`/`}>
-            Let's Start A Project
-          </Link>
-        </section>
       </nav>
+      <div className={styles.mobileNav} ref={scope}>
+        <Lottie
+          lottieRef={burgerRef}
+          loop={false}
+          autoplay={false}
+          animationData={Hamburger}
+          onClick={handleClick}
+          onEnterFrame={handleEnterFrame}
+          className={styles.hamburger}
+          priority
+        />
+        <nav className={styles.navWrap} animate={isOpen ? "open" : "closed"}>
+          <span>
+            <Link href={`/`} className={styles.navLink}>
+              Websites
+            </Link>
+          </span>
+          <span>
+            <Link href={`/`} className={styles.navLink}>
+              Discord Bots
+            </Link>
+          </span>
+          <span>
+            <Link href={`/`} className={styles.navLink}>
+              About
+            </Link>
+          </span>
+          <section className={styles.mobContact}>
+            <h3>something here</h3>
+            <section>
+              <p>email here</p>
+              <p>more text</p>
+            </section>
+            <Link className={styles.mobBtn} href={`/`}>
+              Let's Start A Project
+            </Link>
+          </section>
+        </nav>
+      </div>
     </header>
   );
 };
